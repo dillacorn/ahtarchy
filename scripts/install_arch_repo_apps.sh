b999ddc2124bd8fe0adf8c1a0e43514795d1af95
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-05-02 11:03:35 -0400

    Update install_arch_repo_apps.sh

--- scripts/install_arch_repo_apps.sh
+++ scripts/install_arch_repo_apps.sh
@@ -222,7 +222,6 @@ fi
 
     # Print success message after installation
     echo -e "\n${GREEN}Successfully installed all of Dillacorn's Arch Linux chosen applications!${NC}"
-fi
 else
     echo -e "\n${YELLOW}Skipping installation of Dillacorn's chosen Arch Repo Linux applications.${NC}"
 fi
