fa5ba4017585ab6e09f83da0e5556b964bc4d968
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-05-08 11:02:30 -0400

    Update install_flatpak_apps.sh
    added Kooha back.. it's actually a great piece of recording software for simple tasks

--- scripts/install_flatpak_apps.sh
+++ scripts/install_flatpak_apps.sh
@@ -47,6 +47,7 @@ flatpak_apps=(
   'com.github.tchx84.Flatseal'
   'dev.deedles.Trayscale'
   'com.github.wwmm.easyeffects'
+  'io.github.seadve.Kooha'
 )
 
 # Detect file system type of the root partition
