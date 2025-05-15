a8fe0be5160dd28071bb38f1311fdd5dda73f3a3
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-05-14 20:43:23 -0400

    Update install_aur_repo_apps.sh
    removed cava.. idk why it's in here it's installed from main repo.. mistake

--- scripts/install_aur_repo_apps.sh
+++ scripts/install_aur_repo_apps.sh
@@ -68,7 +68,6 @@ if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
         # List of AUR packages to install with cleanup
         packages=(
             qimgv
-            cava
             otpclient
             wlogout
             waypaper
