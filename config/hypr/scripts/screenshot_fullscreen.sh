6b24a02d87c431cef0e5bedecb8756543cbda205
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-05-24 11:12:20 -0400

    Update screenshot_fullscreen.sh
    added no border for fullscreen screenshots.. just noticed it was picking up the boarder.

--- config/hypr/scripts/screenshot_fullscreen.sh
+++ config/hypr/scripts/screenshot_fullscreen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GEOM=$(slurp -o -r -c '#ff0000ff')
+GEOM=$(slurp -o -r -c '#00000000')
 [ -z "$GEOM" ] && exit 1
 
 grim -g "$GEOM" -t ppm - | satty --filename - --fullscreen --output-filename "$HOME/Pictures/Screenshots/$(date +%m%d%Y-%I%p-%S).png"
