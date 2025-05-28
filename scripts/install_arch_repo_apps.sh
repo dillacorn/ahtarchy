785c7c80c5badfbfe93c313306c15851d88b90f6
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-05-28 11:13:46 -0400

    Update install_arch_repo_apps.sh

--- scripts/install_arch_repo_apps.sh
+++ scripts/install_arch_repo_apps.sh
@@ -101,9 +101,9 @@ declare -a pkg_groups=(
     "Fonts:ttf-font-awesome ttf-hack ttf-dejavu ttf-liberation noto-fonts"
     "Themes:papirus-icon-theme materia-gtk-theme xcursor-comix"
     "Terminal Apps:micro alacritty fastfetch btop htop curl wget git dos2unix brightnessctl ipcalc cmatrix sl asciiquarium figlet cava man-db man-pages unzip octave"
-    "Utilities:steam polkit-kde-agent lxappearance networkmanager network-manager-applet tailscale bluez bluez-utils blueman pavucontrol pcmanfm gvfs gvfs-smb gvfs-mtp gvfs-afc qbittorrent speedcrunch partitionmanager filelight timeshift imagemagick pipewire pipewire-pulse pipewire-alsa ufw"
+    "Utilities:steam polkit-gnome lxappearance networkmanager network-manager-applet tailscale bluez bluez-utils blueman pavucontrol pcmanfm gvfs gvfs-smb gvfs-mtp gvfs-afc qbittorrent speedcrunch timeshift imagemagick pipewire pipewire-pulse pipewire-alsa ufw"
     "Multimedia:ffmpeg avahi mpv cheese exiv2 audacity krita shotcut filezilla gthumb handbrake"
-    "Development:base-devel archlinux-keyring clang ninja go rust okular bleachbit virt-manager qemu virt-viewer vde2 libguestfs dmidecode gamemode nftables swtpm"
+    "Development:base-devel archlinux-keyring clang ninja go rust virt-manager qemu virt-viewer vde2 libguestfs dmidecode gamemode nftables swtpm"
     "Network Tools:wireguard-tools wireplumber openssh iptables systemd-resolvconf bridge-utils qemu-guest-agent dnsmasq dhcpcd inetutils openbsd-netcat"
 )
 
