0145c70dfd2455148b080d013773bd4fd39c7b39
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-07-09 13:48:43 -0400

    tired of trying to hide the UI with CTRL+t for screenshot area in satty.. keeping the editing tools for fullscreen screenshots though

--- config/hypr/scripts/screenshot_area.sh
+++ config/hypr/scripts/screenshot_area.sh
@@ -3,4 +3,7 @@
 GEOM=$(slurp -b '#ffffff20' -c '#00000040')
 [ -z "$GEOM" ] && exit 1
 
-grim -g "$GEOM" -t ppm - | satty --filename - --output-filename "$HOME/Pictures/Screenshots/$(date +%m%d%Y-%I%p-%S).png"
+grim -g "$GEOM" -t ppm - | satty \
+  --filename - \
+  --output-filename "$HOME/Pictures/Screenshots/$(date +%m%d%Y-%I%p-%S).png" \
+  --default-hide-toolbars
