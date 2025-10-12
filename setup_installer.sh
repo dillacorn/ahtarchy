4d18f51dd0e4744cd2e6bca7c64f09c30a19a3a1
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-10-12 22:25:17 +0000

    added wiremix config.. now behaves similarly to pulsemixer! :) - This is the best audio TUI tool!

--- setup_installer.sh
+++ setup_installer.sh
@@ -98,6 +98,7 @@ echo -e "${COLOR_YELLOW}
 - ~/.config/xdg-desktop-portal
 - ~/.config/lsfg-vk
 - ~/.config/YouTube Music
+- ~/.config/wiremix
 - ~/.Xresources
 - ~/.local/share/nwg-look/gsettings${COLOR_RESET}"
 echo -e "${COLOR_RED}Are you sure you want to continue? This action CANNOT be undone.${COLOR_RESET}"
@@ -196,7 +197,7 @@ done
 # Copy all configuration files
 echo -e "${COLOR_BLUE}Copying configuration files...${COLOR_RESET}"
 config_dirs=("hypr" "waybar" "alacritty" "wlogout" "mako" "wofi"
-    "gtk-3.0" "Kvantum" "SpeedCrunch" "fastfetch" "pcmanfm-qt" "yazi" "xdg-desktop-portal" "qt5ct" "qt6ct" "lsfg-vk" "YouTube Music")
+    "gtk-3.0" "Kvantum" "SpeedCrunch" "fastfetch" "pcmanfm-qt" "yazi" "xdg-desktop-portal" "qt5ct" "qt6ct" "lsfg-vk" "wiremix" "YouTube Music")
 
 for dir in "${config_dirs[@]}"; do
     retry_command cp -r "$REPO_DIR/config/$dir" "$HOME_DIR/.config/" || exit 1
